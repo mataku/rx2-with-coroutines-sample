1388dd6ba61540ef204413d3865c58fe95dcbd40
Takuma Homma <nagomimatcha@gmail.com>
2020-07-19 22:59:45 +0900

    Specify correct query parameters

--- app/src/main/java/com/mataku/rx2_with_coroutines_sample/model/entity/TopArtistsApiService.kt
+++ app/src/main/java/com/mataku/rx2_with_coroutines_sample/model/entity/TopArtistsApiService.kt
@@ -6,14 +6,14 @@ import retrofit2.http.GET
 
 
 interface RxApiService {
-    @GET("/2.0/?method=chart.gettopartists?format=json")
+    @GET("/2.0/?method=chart.gettopartists&format=json")
     fun getTopArtists(
         apiKey: String,
         limit: Int,
         page: Int
     ): Single<Response<TopArtistsApiResponse>>
 
-    @GET("/2.0/?method=chart.gettoptracks?format=json")
+    @GET("/2.0/?method=chart.gettoptracks&format=json")
     fun getTopTracks(
         apiKey: String,
         limit: Int,
--- app/src/main/java/com/mataku/rx2_with_coroutines_sample/model/service/LastFmApiService.kt
+++ app/src/main/java/com/mataku/rx2_with_coroutines_sample/model/service/LastFmApiService.kt
@@ -4,19 +4,20 @@ import com.mataku.rx2_with_coroutines_sample.model.entity.TopArtistsApiResponse
 import com.mataku.rx2_with_coroutines_sample.model.entity.TopTracksApiResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CoroutinesApiService {
-    @GET("/2.0/?method=chart.gettopartists?format=json")
+    @GET("/2.0/?method=chart.gettopartists&format=json")
     suspend fun getTopArtists(
-        apiKey: String,
-        limit: Int,
-        page: Int
+        @Query("api_key") apiKey: String,
+        @Query("limit") limit: Int,
+        @Query("page") page: Int
     ): Response<TopArtistsApiResponse>
 
-    @GET("/2.0/?method=chart.gettoptracks?format=json")
+    @GET("/2.0/?method=chart.gettoptracks&format=json")
     suspend fun getTopTracks(
-        apiKey: String,
-        limit: Int,
-        page: Int
+        @Query("api_key") apiKey: String,
+        @Query("limit") limit: Int,
+        @Query("page") page: Int
     ): Response<TopTracksApiResponse>
 }
