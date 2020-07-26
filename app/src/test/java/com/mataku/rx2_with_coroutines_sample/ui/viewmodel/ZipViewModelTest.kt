3a68afd6f9bf59580b3a7953aebbe659ba0133eb
Takuma Homma <nagomimatcha@gmail.com>
2020-07-26 22:57:02 +0900

    Kick unused class

--- app/src/test/java/com/mataku/rx2_with_coroutines_sample/ui/viewmodel/ZipViewModelTest.kt
+++ app/src/test/java/com/mataku/rx2_with_coroutines_sample/ui/viewmodel/ZipViewModelTest.kt
@@ -7,47 +7,13 @@ import com.mataku.rx2_with_coroutines_sample.model.entity.Track
 import com.mataku.rx2_with_coroutines_sample.model.presentation.NetworkResult
 import com.mataku.rx2_with_coroutines_sample.model.repository.TopArtistsRepository
 import com.mataku.rx2_with_coroutines_sample.model.repository.TopTracksRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
-import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runBlockingTest
-import kotlinx.coroutines.test.setMain
 import org.junit.Assert
 import org.junit.Rule
-import org.junit.rules.TestRule
-import org.junit.runner.Description
-import org.junit.runners.model.Statement
 import org.mockito.Mockito
-import kotlin.test.AfterTest
-import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertNotNull
-
-@ExperimentalCoroutinesApi
-class TestCoroutineRule : TestRule {
-
-    private val testCoroutineDispatcher = TestCoroutineDispatcher()
-
-    private val testCoroutineScope = TestCoroutineScope(testCoroutineDispatcher)
-
-    override fun apply(base: Statement, description: Description?) = object : Statement() {
-        @Throws(Throwable::class)
-        override fun evaluate() {
-            Dispatchers.setMain(testCoroutineDispatcher)
-
-            base.evaluate()
-
-            Dispatchers.resetMain()
-            testCoroutineScope.cleanupTestCoroutines()
-        }
-    }
-
-    fun runBlockingTest(block: suspend TestCoroutineScope.() -> Unit) =
-        testCoroutineScope.runBlockingTest { block() }
-
-}
 
 @ExperimentalCoroutinesApi
 class ZipViewModelTest {
@@ -55,12 +21,6 @@ class ZipViewModelTest {
     @Rule
     @JvmField
     val instantTaskExecutorRule = InstantTaskExecutorRule()
-
-    @Rule
-    @JvmField
-    val coroutinesRule = TestCoroutineRule()
-
-    private val testDispatcher = TestCoroutineDispatcher()
 
     private val mockArtist = Artist(
         imageList = listOf(),
@@ -79,16 +39,6 @@ class ZipViewModelTest {
         url = "https://www.last.fm/ja/music/PassCode/_/Taking+You+Out",
         playcount = 1
     )
-
-    @BeforeTest
-    fun setup() {
-        Dispatchers.setMain(testDispatcher)
-    }
-
-    @AfterTest
-    fun tearDown() {
-        Dispatchers.resetMain()
-    }
 
     @Test
     fun request_success() {
