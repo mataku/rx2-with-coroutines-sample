1002c784d95a85f75d7839f1896a14fb6d9f0c45
Takuma Homma <nagomimatcha@gmail.com>
2020-07-26 22:57:28 +0900

    Repository test

---
+++ app/src/test/java/com/mataku/rx2_with_coroutines_sample/model/repository/TopArtistsRepositoryTest.kt
@@ -0,0 +1,71 @@
+package com.mataku.rx2_with_coroutines_sample.model.repository
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.mataku.rx2_with_coroutines_sample.model.entity.Artist
+import com.mataku.rx2_with_coroutines_sample.model.entity.TopArtists
+import com.mataku.rx2_with_coroutines_sample.model.entity.TopArtistsApiResponse
+import com.mataku.rx2_with_coroutines_sample.model.presentation.NetworkResult
+import com.mataku.rx2_with_coroutines_sample.model.service.CoroutinesApiService
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Rule
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import retrofit2.Response
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+@ExperimentalCoroutinesApi
+class TopArtistsRepositoryTest {
+
+    @Rule
+    @JvmField
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var apiService: CoroutinesApiService
+
+    private val apiKey = "test"
+    private val limit = 1
+    private val page = 1
+
+    @BeforeTest
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+
+    }
+
+    private val topArtists = TopArtists(
+        listOf(
+            Artist(
+                name = "PassCode",
+                imageList = emptyList(),
+                url = "https://example.com"
+            )
+        )
+    )
+
+    private val mockResponse = TopArtistsApiResponse(topArtists)
+
+    @Test
+    fun getTopArtists_success() = runBlockingTest {
+        Mockito.`when`(
+            apiService.getTopArtists(
+                apiKey = apiKey,
+                limit = limit,
+                page = page
+            )
+        ).thenReturn(
+            Response.success(mockResponse)
+        )
+
+        val repo = TopArtistsRepository(apiService = apiService)
+        val result = repo.getTopArtists()
+        assertTrue(result is NetworkResult.Success)
+        assertNotNull(result.getOrNull())
+    }
+
+}
