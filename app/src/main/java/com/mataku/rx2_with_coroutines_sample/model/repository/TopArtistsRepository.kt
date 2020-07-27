19fc0f7c5798e10418bca9b6674d8ac8513bccc6
Takuma Homma <nagomimatcha@gmail.com>
2020-07-27 14:00:03 +0900

    Use arguments

--- app/src/main/java/com/mataku/rx2_with_coroutines_sample/model/repository/TopArtistsRepository.kt
+++ app/src/main/java/com/mataku/rx2_with_coroutines_sample/model/repository/TopArtistsRepository.kt
@@ -1,6 +1,5 @@
 package com.mataku.rx2_with_coroutines_sample.model.repository
 
-import com.mataku.rx2_with_coroutines_sample.BuildConfig
 import com.mataku.rx2_with_coroutines_sample.model.entity.Artist
 import com.mataku.rx2_with_coroutines_sample.model.presentation.NetworkResult
 import com.mataku.rx2_with_coroutines_sample.model.presentation.TimeoutException
@@ -14,9 +13,7 @@ class TopArtistsRepository(private val apiService: CoroutinesApiService) {
     ): NetworkResult<List<Artist>> {
         return try {
             val result = apiService.getTopArtists(
-                apiKey = BuildConfig.API_KEY,
-                limit = 3,
-                page = 1
+                apiKey, limit, page
             )
             val body = result.body()
             if (result.isSuccessful && body != null) {
